ed0a1bb59157ac2ab2bbc14479ec1a22187ca029
slingr011 <60192182+slingr011@users.noreply.github.com>
2020-02-14 21:23:52 -0600

    Add files via upload

---
+++ Dog/Dog/Dog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Dog
+{
+
+    public enum Gender
+    {
+        Male,
+        Female
+
+    }
+    class Dog
+    {
+        public static string name;
+        public static string owner;
+        public static int age;
+        public static Gender _gender;
+        public Dog(string dogname, string ownername, int dogage, Gender gender)
+        {
+            name = dogname;
+            owner = ownername;
+            age = dogage;
+            _gender = gender;
+        }
+        public void Bark(int x)
+        {
+            int i = 1;
+            while (i<=x)
+            {
+                Console.Write("Woof!");
+                i++;
+            }
+        }
+        public bool GetTag()
+        {
+            string sex;
+            string gender;
+            string year = "years";
+            if(Dog._gender == 0)
+            {
+                sex = "His";
+                gender = "he";
+            }
+            else
+            {
+                sex = "Her";
+                gender = "she";
+            }
+            if(Dog.age == 1)
+            {
+                year = "year";
+            }
+            Console.WriteLine($"\nIf lost, call {Dog.owner}.  {sex} name is {Dog.name} and {gender} is {Dog.age} {year} old.");
+            return true;
+        }
+    }
+}
---
+++ Dog/Dog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog puppy = new Dog("Orian", "Shawn", 1, Gender.Male);
+            puppy.Bark(3);
+            puppy.GetTag();
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.Bark(1);
+            myDog.GetTag();
+        }
+    }
+}
